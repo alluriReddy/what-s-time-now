6fc733639fd423363afd24656164f840f31afa41
alluriReddy <116654732+alluriReddy@users.noreply.github.com>
2022-11-11 22:22:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-//const add = (
-    
-  //<div>
-    //<img src=" https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="" width="80px"></img>
-    //<h1>Hello i am React!</h1>
-  //</div>
-//)
-//ReactDOM.render(add,document.querySelector("body"))
+
 function App() {
   const date = new Date()
   const hours = date.getHours() % 12
@@ -17,4 +10,4 @@ function App() {
   )
 }
 
-ReactDOM.render(<App />, document.getElementById("root"))
+
